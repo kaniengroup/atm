926a01bc2515ee94b883de0593912902f561dfa3
AZATASSOU <azatassulam11@gmail.com>
2019-10-01 08:39:48 +0000

    pour test

--- application/views/pages/agenda.php
+++ application/views/pages/agenda.php
@@ -1,8 +1,8 @@
 
 
 <div class="container">
-    <div class="row">  
-        <img class="img-fluid" src="<?php echo base_url('assets/img/sliders/slide1.jpg'); ?>" alt="First slide">           
+    <div class="row">
+        <img class="img-fluid" src="<?php echo base_url('assets/img/sliders/slide1.jpg'); ?>" alt="First slide">
     </div>
     <div class="container">
         &nbsp;
@@ -20,27 +20,27 @@
                     <div class="row">
                         <div class="col-md-7 text-justify text_who_is ">
                             <!-- <div class="col-md-12 bg_article_liste pt-3 pb-3">
-                                <?php 
+                                <?php
                                 if ($nbre_article_total>0) {
                                 ?>
                                     <?php
                                     $valeur_finale = -1;
-                                    for ($i=1; $i <= $nbre_page; $i++) { 
-                                       
+                                    for ($i=1; $i <= $nbre_page; $i++) {
+
                                     ?>
                                         <div id="<?= 'page_'.$i; ?>" class="row view_page">
                                         <?php
                                         $valeur_initiale = $valeur_finale + 1;
                                         $valeur_finale = $valeur_initiale + ($nbre_article_par_page - 1);
                                         if ($i==$nbre_page) {
-                                            $valeur_finale = $nbre_article_total - 1; 
+                                            $valeur_finale = $nbre_article_total - 1;
                                         }
                                         for ($j=$valeur_initiale; $j <= $valeur_finale; $j++) {
                                         ?>
                                             <div class="card m-3">
                                                 <div class="row no-gutters">
                                                     <div class="col-md-4">
-                                                        
+
                                                     30/09/2019
                                                     </div>
                                                     <div class="col-md-8">
@@ -53,12 +53,12 @@
                                                                 <button type="button" class="btn btn-primary btn-sm ">Lire...</button>
                                                                 <button type="button" class="btn btn-primary btn-sm ">Partager</button>
                                                             </span>
-                                                            
+
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div> 
-                                        <?php   
+                                            </div>
+                                        <?php
                                         }
                                         ?>
                                         </div>
@@ -68,14 +68,14 @@
                                     <div id="page">
                                         <ul class="pagination"></ul>
                                     </div>
-                                <?php    
+                                <?php
                                 }
                                 ?>
-                           
+
                             </div> -->
-                            
+
                             <div class="col-md-12 bg_article_liste mt-3 pt-3 pb-3 ">
-                                
+
                             </div>
                         </div>
                         <div class="col-md-5 text-justify text_who_is bg_article_liste p-3">
